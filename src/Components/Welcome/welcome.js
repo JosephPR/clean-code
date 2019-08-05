db0d483dd81759824f1962e48b8f0712c449a172
JosephPR <reilly4181@gmail.com>
2019-08-05 14:23:53 -0600

    touch up welcome page

--- src/Components/Welcome/welcome.js
+++ src/Components/Welcome/welcome.js
@@ -8,12 +8,18 @@ export default function Welcome() {
   return (
     <>
     <h1 className='hi'>Hello</h1>
-
-    <div className='welcome'>
-    <Link to="/genie"><img src='https://media3.giphy.com/media/jU1vHM8oQBaZHW1bT3/giphy.gif?cid=790b76115d47418f7669484d7327db44&rid=giphy.gif' alt='genie' /></Link>
-      <Link to="/disney"><img src='https://media.giphy.com/media/U51L9vKEbdgyrUeRHM/giphy.gif' alt='alice' /></Link>
-    <img src='https://media1.giphy.com/media/ykvRcYBJBiQwg/200w.webp?cid=790b76115d473e3179546c30493793ff&rid=200w.webp' alt='hello'/>
-  </div>
+    <div className='directory-items'>
+        <div className='titles'>
+          <h3>genie info </h3>
+          <h3>Alice in Wonderland</h3>
+          <h3>New Movies</h3>
+        </div>
+        <div className='welcome'>
+          <Link to="/genie"><img src='https://media3.giphy.com/media/jU1vHM8oQBaZHW1bT3/giphy.gif?cid=790b76115d47418f7669484d7327db44&rid=giphy.gif' alt='genie' /></Link>
+          <Link to="/disney"><img className='drink-me' src='https://media.giphy.com/media/U51L9vKEbdgyrUeRHM/giphy.gif' alt='alice' /></Link>
+          <Link to="/playing"><img src='https://media1.giphy.com/media/ykvRcYBJBiQwg/200w.webp?cid=790b76115d473e3179546c30493793ff&rid=200w.webp' alt='hello'/></Link>
+        </div>
+    </div>
 
 
 
